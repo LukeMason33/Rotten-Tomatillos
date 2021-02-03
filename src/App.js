83839a0deeff525d15a3704e771f2b2760f9d75c
Luke Mason <lukjmason@gmail.com>
2021-02-03 16:28:15 -0700

    Create movie component files and folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import movieData from './data.js';
+import MovieContainer from './movie-components/movie-container-component/movie-container.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
+      movies: movieData.movies,
+      singleMovieView: false
     }
   }
 
   render () {
-    return(
-      <h1>Here</h1>
+    return (
+      <main className="main-dashboard">
+        // insert header component here
+        <h1>Here</h1>
+        <section className="movie-container">
+        < MovieContainer movies={...this.state} />
+        </section>
+      </main>
     )
   }
 }
