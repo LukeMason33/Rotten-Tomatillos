4f9414ef5e2ed64fbf513f980ea75b94771ff6a9
Luke Mason <lukjmason@gmail.com>
2021-02-03 16:10:43 -0700

    Insert movie data into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import movieData from './data.js';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      movies: movieData.movies
+    }
+  }
 
-    this.state =
+  render () {
+    return(
+      <h1>Here</h1>
+    )
   }
 }
+
+export default App;
