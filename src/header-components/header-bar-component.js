0165748f37676f6bd44a212e35cbbc72e6df1657
Luke Mason <lukjmason@gmail.com>
2021-02-04 13:33:41 -0700

    Create header seach bar component

--- src/header-components/header-bar-component.js
+++ src/header-components/header-bar-component.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeaderSearch from './header-search-component.js';
 
 function Header() {
   return (
     <header className="header">
       <h1>Rotten Tomatillos</h1>
+      <HeaderSearch />
     </header>
   )
 }
