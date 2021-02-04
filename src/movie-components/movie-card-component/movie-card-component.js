81f6287bf5a9890684f13f1f275116a5187cf87a
Luke Mason <lukjmason@gmail.com>
2021-02-03 17:25:17 -0700

    Pass information thorugh movie card component to display wanted info

--- src/movie-components/movie-card-component/movie-card-component.js
+++ src/movie-components/movie-card-component/movie-card-component.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 
 function MovieCard (props) {
-  console.log(props);
   return (
     <div className='movie-card'>
-      <img src={props}/>
+      <h1>{props.title}</h1>
+      <img src={props.poster_path}/>
+      <div>
+        <h2>Genre</h2>
+        <p>*Insert Genre Here*</p>
+      </div>
+      <div>
+        <h2>Rating</h2>
+        <p>{props.average_rating.toFixed(2)}</p>
+      </div>
     </div>
   )
 }
