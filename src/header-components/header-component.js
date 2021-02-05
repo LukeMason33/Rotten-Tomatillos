3c779ba44ff49d9def481cf793a24a7317e6e2bc
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-02-05 15:40:51 -0700

    Minor header element changes

--- src/header-components/header-component.js
+++ src/header-components/header-component.js
@@ -4,8 +4,9 @@ import HeaderFilter from './filter-menu-component/filter-component.js';
 function Header(props) {
   return (
     <header className="header">
-      <h1>Rotten Tomatillos
-        <div></div>
+      <h1 className="logo">
+        <div ></div>
+        <h1>Rotten Tomatillos</h1>
       </h1>
       <HeaderSearch onChange={props.onChange}/>
       <HeaderFilter/>
